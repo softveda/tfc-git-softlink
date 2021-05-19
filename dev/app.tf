12e795bf95c5f71a5bc8dc1aaac5219effcc47ae
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2021-05-19 16:44:41 +1000

    Adding app vnet resource

---
+++ dev/app.tf
@@ -0,0 +1,11 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "app-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+}
