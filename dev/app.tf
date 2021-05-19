8b4219ca0ff2608795eb2e1ae44129729eb415ac
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2021-05-19 20:42:41 +1000

    Trigger Build

--- dev/app.tf
+++ dev/app.tf
@@ -9,9 +9,4 @@ resource "azurerm_virtual_network" "vnet" {
     name           = "subnet1"
     address_prefix = "10.0.1.0/24"
   }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-  }
 }
