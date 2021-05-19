a9adfd9c3ed7f5999a665fa77d195def54ef5389
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2021-05-19 20:33:50 +1000

    Adding qa files

--- qa/app.tf
+++ qa/app.tf
@@ -1,6 +1,6 @@
 # App specific code 01
 resource "azurerm_virtual_network" "vnet" {
-  name                = "app-vnet-qa"
+  name                = "app-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.1.0.0/16"]
